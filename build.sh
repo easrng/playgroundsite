3bb4c9086b05ce21cc902be7d31d9e89dcc603bd
easrng <easrng@gmail.com>
2018-12-11 14:18:34 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 npm install shoelace-css
 npm install postcss postcss-cssnext
 npm i postcss-cli
-node_modules/postcss-cli/postcss node_modules/shoelace-css/source --dir shoelace
+node_modules/postcss/postcss node_modules/shoelace-css/source --dir shoelace
 echo
 echo
 ls */*/*/*/* -l | grep postcss-cli > list.txt
