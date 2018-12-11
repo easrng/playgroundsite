f77e830e722d4596b64cb8772912131aae32f28f
easrng <easrng@gmail.com>
2018-12-11 14:04:53 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 npm install shoelace-css
 npm install postcss postcss-cssnext
 npm i postcss-cli
-postcss shoelacesrc --dir shoelace
+node_modules/postcss-cli/postcss node_modules/shoelace-css/source --dir shoelace
 ls */*/*/*/* -l > list.txt
